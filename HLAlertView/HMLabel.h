2d734f394afe412a56027119f5234a88b1d4ba06
liangmingzhe <benjaminlmz@qq.com>
2020-05-11 14:47:45 +0800

    first commit

---
+++ HLAlertView/HMLabel.h
@@ -0,0 +1,17 @@
+//
+//  HMLabel.h
+//  HLAlertController
+//
+//  Created by benjaminlmz@qq.com on 2020/5/9.
+//  Copyright © 2020 Tony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HLAlertModel.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HMLabel : NSObject
++ (instancetype)labelWithTitle:(NSString *)title block:(void(^)(Constraint *constraint,HLTextModel *textModel))block;
+@end
+
+NS_ASSUME_NONNULL_END
